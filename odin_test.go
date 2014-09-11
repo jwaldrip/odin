3aaaa0840e13608268cfd1122e90c815caca89b5
Jason Waldrip <jwaldrip@Jasons-MacBook-Pro.local>
2014-09-10 22:27:16 -0600

    more work on flag parsing and starting

---
+++ odin_test.go
@@ -0,0 +1,12 @@
+package odin_test
+
+import (
+	. "github.com/jwaldrip/odin"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Odin", func() {
+	var c Command
+	
+})
