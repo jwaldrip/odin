bc69f1eeb5891038cbf90bd4617ac02bab9efa35
Jason Waldrip <jason@waldrip.net>
2014-09-23 22:16:33 -0600

    gofmt

--- cli/errors.go
+++ cli/errors.go
@@ -1,17 +1,17 @@
 package cli
-
-import . "log"
-import "os"
-
-var stderr = New(os.Stderr, "", 0)
-var stdout = New(os.Stdout, "", 0)
-
-func exitIfError(error error) {
-	if error != nil {
-		stderr.Fatalln(error)
-	}
-}
-
-func fail(msg string) {
-	stderr.Fatalln(msg)
-}
+//
+// import . "log"
+// import "os"
+//
+// var stderr = New(os.Stderr, "", 0)
+// var stdout = New(os.Stdout, "", 0)
+//
+// func exitIfError(error error) {
+// 	if error != nil {
+// 		stderr.Fatalln(error)
+// 	}
+// }
+//
+// func fail(msg string) {
+// 	stderr.Fatalln(msg)
+// }
