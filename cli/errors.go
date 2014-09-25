f279c0801e6aa9af34884d1cde0b7eb98e7132ec
Jason Waldrip <jason@waldrip.net>
2014-09-25 11:10:52 -0600

    dont use log package

--- cli/errors.go
+++ cli/errors.go
@@ -1,13 +1,13 @@
 package cli
 
-import "log"
-import "os"
-
-var stderr = log.New(os.Stderr, "", 0)
-var stdout = log.New(os.Stdout, "", 0)
+import (
+	"fmt"
+	"os"
+)
 
 func exitIfError(err error) {
 	if err != nil {
-		stderr.Fatalln(err)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
 	}
 }
