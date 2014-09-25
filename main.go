1c10ac8777efdfaf0a28d3cdbdc2409c75a664e2
Jason Waldrip <jason@waldrip.net>
2014-09-25 10:40:42 -0600

    Change the version

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import odin "github.com/jwaldrip/odin/cli"
 
-var cli = odin.NewCLI(func(cmd odin.Command) { cmd.Usage() })
+// VERSION is the odin version
+var VERSION = "0.9.0"
 
-func init() {
-	cli.SetVersion("1.0.0")
-}
+var cli = odin.NewCLI(VERSION, "a command line DSL for go-lang", func(cmd odin.Command) { cmd.Usage() })
 
 func main() {
 	cli.Start()
