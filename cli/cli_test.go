03394afdbb3cd29e56255e9fdf673d4e1c6fe54e
Jason Waldrip <jason@waldrip.net>
2014-09-27 11:15:42 -0600

    Duration Values

--- cli/cli_test.go
+++ cli/cli_test.go
@@ -5,6 +5,8 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	"time"
 )
 
 var _ = Describe("CLI", func() {
@@ -41,6 +43,24 @@ var _ = Describe("CLI", func() {
 				Expect(cmd.Flag("boolflag2").Get()).To(Equal(true))
 			})
 		})
+
+		Describe("DurationFlag", func() {
+
+			It("should set and parse", func() {
+				cli.DefineDurationFlag("durflag", time.Duration(60)*time.Second, "desc")
+				cli.Start("cmd", "--durflag=30s")
+				Expect(cmd.Flag("durflag").Get()).To(Equal(time.Duration(30) * time.Second))
+			})
+
+			It("should set and parse from var", func() {
+				var val time.Duration
+				cli.DefineDurationFlagVar(&val, "durflag", time.Duration(60)*time.Second, "desc")
+				cli.DefineDurationFlagVar(&val, "durflag2", time.Duration(45)*time.Second, "desc")
+				cli.Start("cmd", "--durflag=100s")
+				Expect(cmd.Flag("durflag2").Get()).To(Equal(time.Duration(100) * time.Second))
+			})
+		})
+
 	})
 
 	Describe("required parameters", func() {
