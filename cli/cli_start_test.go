e447348ce97eacb8a752f06b8ef39bbcd61ee202
Jason Waldrip <jason@waldrip.net>
2014-09-27 14:32:05 -0600

    test for a complex cli

--- cli/cli_start_test.go
+++ cli/cli_start_test.go
@@ -1,6 +1,8 @@
 package cli_test
 
 import (
+	"strings"
+
 	. "github.com/jwaldrip/odin/cli"
 
 	. "github.com/onsi/ginkgo"
@@ -22,6 +24,64 @@ var _ = Describe("CLI Start", func() {
 		cli = NewCLI("v1.0.0", "sample description", runFn)
 		cli.ErrorHandling = PanicOnError
 		cli.Mute()
+	})
+
+	Describe("complex cli", func() {
+
+		var subDidRun bool
+		var subCmd *SubCommand
+
+		BeforeEach(func() {
+			cli.DefineParams("host", "path")
+			cli.DefineBoolFlag("ssl", false, "do it over ssl")
+			cli.AliasFlag('S', "ssl")
+			cli.DefineStringFlag("username", "", "the username")
+			cli.AliasFlag('u', "username")
+			cli.DefineStringFlag("password", "", "the password")
+			cli.AliasFlag('p', "password")
+			cli.DefineIntFlag("port", 80, "the port")
+			cli.AliasFlag('P', "port")
+			subCmd = cli.DefineSubCommand("do", "what action to do", func(c Command) { cmd = c; subDidRun = true }, "action")
+		})
+
+		It("should parse the main command properly", func() {
+			cli.Start(strings.Split("cmd -Su=wally -p App1etw0 --port 3001 example.com /", " ")...)
+			Expect(cmd.Param("host").Get()).To(Equal("example.com"))
+			Expect(cmd.Param("path").Get()).To(Equal("/"))
+			Expect(cmd.Flag("port").Get()).To(Equal(3001))
+			Expect(cmd.Flag("username").Get()).To(Equal("wally"))
+			Expect(cmd.Flag("password").Get()).To(Equal("App1etw0"))
+			Expect(cmd.Flag("ssl").Get()).To(Equal(true))
+			Expect(didRun).To(Equal(true))
+		})
+
+		It("should parse the sub command properly", func() {
+			subCmd.DefineBoolFlag("power", false, "with power")
+			cli.Start(strings.Split("cmd -Su=wally -p App1etw0 --port 3001 example.com / do --power something", " ")...)
+			Expect(cmd.Parent().Param("host").Get()).To(Equal("example.com"))
+			Expect(cmd.Parent().Param("path").Get()).To(Equal("/"))
+			Expect(cmd.Parent().Flag("port").Get()).To(Equal(3001))
+			Expect(cmd.Parent().Flag("username").Get()).To(Equal("wally"))
+			Expect(cmd.Parent().Flag("password").Get()).To(Equal("App1etw0"))
+			Expect(cmd.Parent().Flag("ssl").Get()).To(Equal(true))
+			Expect(cmd.Flag("power").Get()).To(Equal(true))
+			Expect(cmd.Param("action").Get()).To(Equal("something"))
+			Expect(subDidRun).To(Equal(true))
+		})
+
+		Describe("version", func() {
+			It("should not panic", func() {
+				cli.Start("cmd", "--version")
+			})
+		})
+
+		Describe("help", func() {
+			It("should not panic", func() {
+				cli.Start(strings.Split("cmd --help", " ")...)
+				cli.Start(strings.Split("cmd host path do --help", " ")...)
+			})
+		})
+
 	})
 
 	Describe("required parameters", func() {
