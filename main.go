15cce5ea49a51da8f725705c4668ab81b43ea63c
Jason Waldrip <jason@waldrip.net>
2014-09-27 15:30:54 -0600

    version 1.0.0

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import odin "github.com/jwaldrip/odin/cli"
 
 // VERSION is the odin version
-var VERSION = "0.9.0"
+var VERSION = "1.0.0"
 
 var cli = odin.NewCLI(VERSION, "a command line DSL for go-lang", func(cmd odin.Command) { cmd.Usage() })
 
