eaeab1b43f55d8bec15fd2f7cd47cc602745e8bf
Jason Waldrip <jason@waldrip.net>
2014-09-28 13:21:14 -0600

    add some access tests

--- cli/flag_access_test.go
+++ cli/flag_access_test.go
@@ -4,24 +4,43 @@ import (
 	. "github.com/jwaldrip/odin/cli"
 
 	. "github.com/onsi/ginkgo"
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Flag Access", func() {
 
 	var cli *CLI
 	var cmd Command
-	var didRun bool
 
 	BeforeEach(func() {
-		didRun = false
 		runFn := func(c Command) {
 			cmd = c
-			didRun = true
 		}
 		cli = NewCLI("v1.0.0", "sample description", runFn)
 		cli.ErrorHandling = PanicOnError
 		cli.Mute()
+
+		cli.DefineBoolFlag("foo", false, "is foo")
+		cli.AliasFlag('o', "foo")
+		cli.DefineStringFlag("bar", "", "what bar are you at?")
+		cli.AliasFlag('r', "bar")
+		cli.DefineBoolFlag("baz", true, "is baz")
+		cli.AliasFlag('z', "baz")
+		cli.Start("cmd")
+	})
+
+	Describe("Flag(string) Value", func() {
+		It("should return a flag's value", func() {
+			Expect(cmd.Flag("foo").Get()).To(Equal(false))
+		})
+	})
+
+	Describe("Flags() map[string]value", func() {
+		It("should return each flags value", func() {
+			for k, v := range cmd.Flags() {
+				Expect(v).To(Equal(cmd.Flag(k)))
+			}
+		})
 	})
 
 })
--- cli/param_access_test.go
+++ cli/param_access_test.go
@@ -4,7 +4,7 @@ import (
 	. "github.com/jwaldrip/odin/cli"
 
 	. "github.com/onsi/ginkgo"
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Param Access", func() {
@@ -19,9 +19,24 @@ var _ = Describe("Param Access", func() {
 			cmd = c
 			didRun = true
 		}
-		cli = NewCLI("v1.0.0", "sample description", runFn)
+		cli = NewCLI("v1.0.0", "sample description", runFn, "foo", "bar")
 		cli.ErrorHandling = PanicOnError
 		cli.Mute()
+		cli.Start("cmd", "a", "b")
+	})
+
+	Describe("Flag(string) Value", func() {
+		It("should return a flag's value", func() {
+			Expect(cmd.Param("foo").Get()).To(Equal("a"))
+		})
+	})
+
+	Describe("Flags() map[string]value", func() {
+		It("should return each flags value", func() {
+			for k, v := range cmd.Params() {
+				Expect(v).To(Equal(cmd.Param(k)))
+			}
+		})
 	})
 
 })
