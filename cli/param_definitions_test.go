cc0b17f4d0f20f5a8aeb3bb09494849b5ee2674e
Jason Waldrip <jason@waldrip.net>
2014-09-28 13:49:49 -0600

    param definitions test

--- cli/param_definitions_test.go
+++ cli/param_definitions_test.go
@@ -4,7 +4,7 @@ import (
 	. "github.com/jwaldrip/odin/cli"
 
 	. "github.com/onsi/ginkgo"
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Param Definitions", func() {
@@ -19,9 +19,26 @@ var _ = Describe("Param Definitions", func() {
 			cmd = c
 			didRun = true
 		}
-		cli = NewCLI("v1.0.0", "sample description", runFn)
+		cli = NewCLI("v1.0.0", "sample description", runFn, "a", "b")
 		cli.ErrorHandling = PanicOnError
 		cli.Mute()
+	})
+
+	Describe("DefineParams", func() {
+		It("should define and parse a param list", func() {
+			cli.DefineParams("c", "d")
+			cli.Start("cmd", "foo", "bar")
+			Expect(cmd.Param("c").Get()).To(Equal("foo"))
+			Expect(cmd.Param("d").Get()).To(Equal("bar"))
+		})
+	})
+
+	Describe("Via Start CLI", func() {
+		It("should define and parse a param list", func() {
+			cli.Start("cmd", "foo", "bar")
+			Expect(cmd.Param("a").Get()).To(Equal("foo"))
+			Expect(cmd.Param("b").Get()).To(Equal("bar"))
+		})
 	})
 
 })
