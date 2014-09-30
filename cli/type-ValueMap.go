072c055685a605889cafc00cf976ed5703d20a07
Jason Waldrip <jason@waldrip.net>
2014-09-30 09:54:44 -0600

    add keys method

--- cli/type-ValueMap.go
+++ cli/type-ValueMap.go
@@ -3,6 +3,15 @@ package cli
 // ValueMap is a map fo values with strings for keys
 type ValueMap map[string]Value
 
+// Keys returns the keys of the value map
+func (v ValueMap) Keys() []string {
+	var keys []string
+	for key := range v {
+		keys = append(keys, key)
+	}
+	return keys
+}
+
 // Values returns a ValueList of all the values in the map
 func (v ValueMap) Values() ValueList {
 	var valueList ValueList
