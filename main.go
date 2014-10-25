0bfc5bf104ab1b290ac9bb0e217accd890123cee
Jason Waldrip <jason@waldrip.net>
2014-10-25 12:04:57 -0600

    bump version

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/jwaldrip/odin/cli"
 
 // VERSION is the odin version
-var VERSION = "1.5.0"
+var VERSION = "1.5.1"
 
 var app = cli.New(VERSION, "a command line DSL for go-lang", cli.ShowUsage)
 
