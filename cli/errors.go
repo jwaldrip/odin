3484481beabab2a51da769f3fdef48699034ec74
Jason Waldrip <jason@waldrip.net>
2014-11-05 11:30:00 -0700

    move errors

--- cli/errors.go
+++ cli/errors.go
@@ -11,3 +11,32 @@ func exitIfError(err error) {
 		os.Exit(2)
 	}
 }
+
+// failf prints to standard error a formatted error and usage message and
+// returns the error.
+func (cmd *CLI) failf(format string, a ...interface{}) error {
+	err := fmt.Errorf(format, a...)
+	fmt.Fprintln(cmd.ErrOutput(), err)
+	fmt.Fprintln(cmd.ErrOutput(), "")
+	cmd.Usage()
+	return err
+}
+
+func (cmd *CLI) errf(format string, a ...interface{}) {
+	cmd.handleErr(cmd.failf(format, a...))
+}
+
+func (cmd *CLI) panicf(format string, a ...interface{}) {
+	panic(cmd.failf(format, a...))
+}
+
+func (cmd *CLI) handleErr(err error) {
+	if err != nil {
+		switch cmd.ErrorHandling {
+		case ExitOnError:
+			os.Exit(2)
+		case PanicOnError:
+			panic(err)
+		}
+	}
+}
