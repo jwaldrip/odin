283aa06868e193b7ecc0763f118848b923bc2eb5
Austin Riendeau <ariendeau@itriagehealth.com>
2014-11-21 16:34:19 -0700

    fixes some documentation

--- cli/subCommandAliases.go
+++ cli/subCommandAliases.go
@@ -1,6 +1,6 @@
 package cli
 
-// AliasName allows you to call the subcommand by other names
+// AliasName allows you to call the subcommand by an alias
 func (cmd *CLI) AliasName(alias, subCommand string) {
 	if cmd.nameAliases == nil {
 		cmd.nameAliases = make(map[string]string)
